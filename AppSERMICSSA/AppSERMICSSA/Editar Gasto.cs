64d82e85b14c21ce950901bdd1255c67d8d5ab39
Nana1423 <emanuel.leite10@outlook.com>
2023-04-12 11:35:13 -0600

    Nuevo Proyecto (sin color)

---
+++ AppSERMICSSA/AppSERMICSSA/Editar Gasto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppSERMICSSA
+{
+    public partial class Editar_Gasto : Form
+    {
+        public Editar_Gasto()
+        {
+            InitializeComponent();
+        }
+    }
+}
