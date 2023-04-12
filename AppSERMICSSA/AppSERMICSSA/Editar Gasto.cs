e72725ba54e097ec97369102d9c32dfaaf13587b
Nana1423 <emanuel.leite10@outlook.com>
2023-04-12 12:43:33 -0600

    Nuevo Proyecto y Editar Gasto Version1

--- AppSERMICSSA/AppSERMICSSA/Editar Gasto.cs
+++ AppSERMICSSA/AppSERMICSSA/Editar Gasto.cs
@@ -16,5 +16,30 @@ namespace AppSERMICSSA
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Editar_Gasto_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
